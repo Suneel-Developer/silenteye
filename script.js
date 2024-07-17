1e83281b961ec2a556eff0310be32f17ebd50423
suneeldeveloper <suneeldeveloper11@gmail.com>
2024-07-17 11:05:11 +0500

    delete vite and add more page

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('.nav-link');
     links.forEach(link => {
         if (link.href === window.location.href) {
-            link.classList.add('text-secondary');
+            link.classList.add('text-[#2C56FE]');
         }
     });
 
@@ -70,8 +70,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 faq.querySelector('.faq-answer').classList.add('hidden');
                 faq.querySelector('.arrow-up').classList.add('hidden');
                 faq.querySelector('.arrow-down').classList.remove('hidden');
-                faq.querySelector('.arrow').classList.remove('bg-black-200');
-                faq.querySelector('.arrow').classList.add('bg-gray-200');
+                faq.querySelector('.arrow').classList.remove('bg-[#222222]');
+                faq.querySelector('.arrow').classList.add('bg-[#222222]');
             });
 
             // If the clicked FAQ item is not open, open it
@@ -80,9 +80,33 @@ document.addEventListener('DOMContentLoaded', function () {
                 item.querySelector('.faq-answer').classList.remove('hidden');
                 arrowUp.classList.remove('hidden');
                 arrowDown.classList.add('hidden');
-                item.querySelector('.arrow').classList.remove('bg-gray-200');
-                item.querySelector('.arrow').classList.add('bg-black-200');
+                item.querySelector('.arrow').classList.remove('bg-[#DDDDDD]');
+                item.querySelector('.arrow').classList.add('bg-[#DDDDDD]');
             }
         });
     });
+});
+
+
+// Up Sell Popup 
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const popup = document.getElementById('upsellPopup');
+    const popupContent = document.getElementById('upSellpopupContent');
+    const getStartedBtn = document.getElementById('getStartedBtn');
+    const closeBtn = document.getElementById('upSellcloseBtn');
+
+    function openPopup() {
+        popup.classList.remove('hidden');
+    }
+
+    function closePopup(event) {
+        if (event.target === popup || event.target === closeBtn) {
+            popup.classList.add('hidden');
+        }
+    }
+
+    getStartedBtn.addEventListener('click', openPopup);
+    popup.addEventListener('click', closePopup);
+    closeBtn.addEventListener('click', closePopup);
 });
